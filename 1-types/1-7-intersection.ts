cff4062a8139aea6689aa04d48f9085c06db4cb7
wslee94 <leemuo123@gmail.com>
2023-11-20 22:36:27 +0900

    1-7 intersection  기초

---
+++ 1-types/1-7-intersection.ts
@@ -0,0 +1,19 @@
+{
+  /**
+   * Intersection Types: &
+   */
+
+  type Student = {
+    name: string;
+    score: number;
+  };
+
+  type Worker = {
+    employeeId: number;
+    work: () => void;
+  };
+
+  function internWork(person: Student & Worker) {
+    console.log(person.name, person.score, person.employeeId, person.work());
+  }
+}
