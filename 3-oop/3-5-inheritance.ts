5b86a06a6129eb9f74d5ab5d608920cd15f93aeb
wslee94 <leemuo123@gmail.com>
2023-11-29 20:38:06 +0900

    3, oop inheritance

---
+++ 3-oop/3-5-inheritance.ts
@@ -0,0 +1,84 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT: number = 7;
+    private coffeeBeans: number;
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    fillCoffeeBeans(coffeeBeans: number) {
+      if (coffeeBeans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+
+      this.coffeeBeans += coffeeBeans;
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= CoffeeMachine.BEANS_GRAM_PER_SHOT * shots;
+    }
+
+    private preheat() {
+      console.log("heating up... 🔥");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots}... ☕️`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+
+    clean() {
+      console.log("cleaning thw machine... 🫧");
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+
+    private steamMilk(): void {
+      console.log("Steaming milk... 🥛");
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  const coffeeMachine = new CoffeeMachine(23);
+  const coffelatteMachine = new CaffeLatteMachine(23, "SSSS");
+
+  const coffee1 = coffeeMachine.makeCoffee(2);
+  const coffee2 = coffelatteMachine.makeCoffee(2);
+
+  console.log(coffee1);
+  console.log(coffee2);
+}
